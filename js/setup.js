1a242ecad8ef200e4bb7af61f1e92cec32c436ae
igorand2018 <qa0208@yandex.ru>
2018-04-14 22:13:44 +0300

    ready version 1

--- js/setup.js
+++ js/setup.js
@@ -3,6 +3,28 @@
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+function getRandomName() {
+  var RandomName = WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)];
+  var RandomSurname = WIZARD_SURNAMES[Math.floor(Math.random() * WIZARD_SURNAMES.length)];
+  return RandomName + ' ' + RandomSurname;
+
+
+}
+
+function getRandomCoatColor() {
+
+  var RandomCoatColor = COAT_COLORS[Math.floor(Math.random() * COAT_COLORS.length)];
+  return RandomCoatColor;
+
+}
+function getRandomEyeColor() {
+  var RandomEyeColor = EYES_COLORS[Math.floor(Math.random() * EYES_COLORS.length)];
+  return RandomEyeColor;
+}
+
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -16,32 +38,37 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 
 var wizards = [
   {
-    name: WIZARD_NAMES[0],
-    coatColor: 'rgb(241, 43, 107)'
+    name: getRandomName(),
+    coatColor: getRandomCoatColor(),
+    eyesColor: getRandomEyeColor()
   },
   {
-    name: WIZARD_NAMES[1],
-    coatColor: 'rgb(215, 210, 55)'
+    name: getRandomName(),
+    coatColor: getRandomCoatColor(),
+    eyesColor: getRandomEyeColor()
   },
   {
-    name: WIZARD_NAMES[2],
-    coatColor: 'rgb(101, 137, 164)'
+    name: getRandomName(),
+    coatColor: getRandomCoatColor(),
+    eyesColor: getRandomEyeColor()
   },
   {
-    name: WIZARD_NAMES[3],
-    coatColor: 'rgb(127, 127, 127)'
+    name: getRandomName(),
+    coatColor: getRandomCoatColor(),
+    eyesColor: getRandomEyeColor()
   }
 ];
 
 
-var renderWizard = function (wizard) {
+function renderWizard(wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
-};
+}
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
