438645202f122e0a10ca85c6fdbf95d2836a2b9b
igorand2018 <qa0208@yandex.ru>
2018-04-24 01:22:26 +0300

    added handlers, focus not working, inputs not ready

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,7 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var WIZARDS_NUMBERS = 4;
 
 function getRandomArrayElement(array) {
@@ -12,7 +13,6 @@ function getRandomArrayElement(array) {
 }
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
 
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 
@@ -47,3 +47,83 @@ for (i = 0; i < wizards.length; i++) {
 similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+
+var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
+
+function onPopupEscPress(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+}
+
+function openPopup() {
+  setup.classList.remove('hidden');
+  setup.addEventListener('keydown', onPopupEscPress);
+}
+
+function closePopup() {
+  setup.classList.add('hidden');
+  setup.removeEventListener('keydown', onPopupEscPress);
+}
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+});
+// Добавим исключение для Esc на инпуте
+var setupUserNameInput = document.querySelector('.setup-user-name');
+setupUserNameInput.addEventListener('focus', function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    openPopup();
+  }
+});
+
+// Изменение цвета глаз персонажа по нажатию
+var WizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+
+function changeEyesColor() {
+  return getRandomArrayElement(EYES_COLORS);
+}
+
+
+WizardEyes.addEventListener('click', function () {
+  WizardEyes.style.fill = changeEyesColor();
+});
+
+// Изменение цвета фаерболов по нажатию
+var fireBall = document.querySelector('.setup-fireball-wrap');
+var fireBallInput = document.querySelector('.setup-fireball-wrap .setup-fireball input');
+function changeFireBallColor() {
+  return getRandomArrayElement(FIREBALL_COLORS);
+}
+
+fireBall.addEventListener('click', function () {
+  fireBall.style.background = changeFireBallColor();
+});
